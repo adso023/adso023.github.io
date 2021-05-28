efb84ed2bffc464425262b90d9ab8d82d69210d7
adso023 <dsouzaadrian24@gmail.com>
2021-05-28 19:32:40 -0400

    Website stuff

---
+++ SEG3125-Module2-Grocery/scripts/grocery.js
@@ -0,0 +1,261 @@
+var lactose = true;
+var nut = true;
+var organic = true;
+var products = [
+  {
+    name: "Apple",
+    price: 0.99,
+    dairy: false,
+    nuts: false,
+    organic: false,
+    image: "images/apple.png",
+  },
+  {
+    name: "Pear (Organic)",
+    price: 1.79,
+    dairy: false,
+    nuts: false,
+    organic: true,
+    image: "images/organicpear.png",
+  },
+  {
+    name: "Peanut Butter (Organic)",
+    price: 5.75,
+    dairy: false,
+    nuts: true,
+    organic: true,
+    image: "images/pb.png",
+  },
+  {
+    name: "Milk",
+    price: 4.0,
+    dairy: true,
+    nuts: false,
+    organic: false,
+    image: "images/milk.png",
+  },
+  {
+    name: "Almond Milk",
+    price: 3.5,
+    dairy: true,
+    nuts: true,
+    organic: false,
+    image: "images/almondmilk.png",
+  },
+  {
+    name: "Cream Cheese",
+    price: 2.99,
+    dairy: true,
+    nuts: false,
+    organic: false,
+    image: "images/creamcheese.png",
+  },
+  {
+    name: "Almonds",
+    price: 8.15,
+    dairy: false,
+    nuts: true,
+    organic: false,
+    image: "images/almonds.png",
+  },
+  {
+    name: "Steak (Organic)",
+    price: 11.25,
+    dairy: false,
+    nuts: false,
+    organic: true,
+    image: "images/steak.png",
+  },
+  {
+    name: "Broccoli (Organic)",
+    price: 3.1,
+    dairy: false,
+    nuts: false,
+    organic: true,
+    image: "images/broccoli.png",
+  },
+  {
+    name: "String Beans",
+    price: 2.79,
+    dairy: false,
+    nuts: false,
+    organic: false,
+    image: "images/stringbeans.png",
+  },
+  {
+    name: "Mint Chocolate Chip Ice Cream",
+    price: 1.5,
+    dairy: true,
+    nuts: false,
+    organic: false,
+    image: "images/icecream.png",
+  },
+  {
+    name: "Pretzels",
+    price: 2.25,
+    dairy: false,
+    nuts: false,
+    organic: false,
+    image: "images/pretzels.png",
+  },
+  {
+    name: "Fruit Loops",
+    price: 4.99,
+    dairy: false,
+    nuts: false,
+    organic: false,
+    image: "images/fruitloops.png",
+  },
+  {
+    name: "Cheddar Block Cheese",
+    price: 3.99,
+    dairy: true,
+    nuts: false,
+    organic: false,
+    image: "images/cheddarcheese.png",
+  },
+  {
+    name: "Multigrain Bread",
+    price: 1.8,
+    dairy: false,
+    nuts: false,
+    organic: false,
+    image: "images/bread.png",
+  },
+  {
+    name: "Butter",
+    price: 4.0,
+    dairy: true,
+    nuts: false,
+    organic: false,
+    image: "images/butter.png",
+  },
+  {
+    name: "Eggs (Organic)",
+    price: 5.0,
+    dairy: false,
+    nuts: false,
+    organic: true,
+    image: "images/eggs.jpg",
+  },
+  {
+    name: "Bananas (Organic)",
+    price: 1.15,
+    dairy: false,
+    nuts: false,
+    organic: true,
+    image: "images/bananas.png",
+  },
+  {
+    name: "Strawberries (Organic)",
+    price: 1.15,
+    dairy: false,
+    nuts: false,
+    organic: true,
+    image: "images/strawberries-package.jpg",
+  },
+  {
+    name: "Pecan Pie",
+    price: 12.0,
+    dairy: false,
+    nuts: true,
+    organic: false,
+    image: "images/pie.png",
+  },
+  {
+    name: "Baby Carrots",
+    price: 2.0,
+    dairy: false,
+    nuts: false,
+    organic: false,
+    image: "images/carrots.png",
+  },
+  {
+    name: "Hummus",
+    price: 3.05,
+    dairy: false,
+    nuts: false,
+    organic: false,
+    image: "images/hummus.png",
+  },
+  {
+    name: "Rotisserie Chicken",
+    price: 10.5,
+    dairy: false,
+    nuts: false,
+    organic: false,
+    image: "images/rotisseriechicken.png",
+  },
+  {
+    name: "Crackers",
+    price: 2.25,
+    dairy: false,
+    nuts: false,
+    organic: false,
+    image: "images/crackers.png",
+  },
+  {
+    name: "Marshmallows",
+    price: 3.05,
+    dairy: false,
+    nuts: false,
+    organic: false,
+    image: "images/marshmallows.png",
+  },
+  {
+    name: "Lemonade",
+    price: 3.55,
+    dairy: false,
+    nuts: false,
+    organic: false,
+    image: "images/lemonade.jpg",
+  },
+  {
+    name: "Rice (Organic)",
+    price: 3.99,
+    dairy: false,
+    nuts: false,
+    organic: true,
+    image: "images/rice.png",
+  },
+  {
+    name: "Spaghetti",
+    price: 2.49,
+    dairy: false,
+    nuts: false,
+    organic: false,
+    image: "images/spaghetti.png",
+  },
+];
+
+var cart = [
+];
+
+function filterProducts() {
+  let productsHere = [...products];
+  if (organic) {
+    productsHere = productsHere.filter((value) => value.organic);
+  } else {
+    productsHere = productsHere.filter((value) => !value.organic);
+  }
+
+  if (! lactose) {
+    productsHere = productsHere.filter((value) => ! value.dairy);
+  }
+
+  if (! nut) {
+    productsHere = productsHere.filter((value) => ! value.nuts);
+  }
+  return productsHere;
+}
+
+function calculateTotal() {
+  let total = 0.00;
+
+  for (let index = 0; index < cart.length; index++) {
+    const element = cart[index];
+    total += element.price;
+  }
+
+  return total;
+}
---
+++ SEG3125-Module2-Grocery/scripts/main.js
@@ -0,0 +1,206 @@
+/**
+ * Changes the navigation stuff
+ * @param {String} nav
+ * @param {String} view
+ */
+function navigateTopLevel(nav, view) {
+  const navItem = document.querySelector("#nav");
+  const navChildren = navItem.children;
+  for (let index = 0; index < navChildren.length; index++) {
+    if (navChildren.item(index).classList.contains("activated")) {
+      navChildren.item(index).classList.remove("activated");
+    }
+
+    if (navChildren.item(index).id === nav) {
+      navChildren.item(index).classList.add("activated");
+    }
+  }
+
+  const viewItems = document.querySelector("main");
+  const viewChildren = viewItems.children;
+  for (let index = 0; index < viewChildren.length; index++) {
+    viewChildren.item(index).classList.replace("in-view", "out-view");
+    if (viewChildren.item(index).id === view) {
+      viewChildren.item(index).classList.remove("out-view");
+      viewChildren.item(index).classList.add("in-view");
+    }
+  }
+}
+
+/**
+ *
+ */
+function profileDropdownChange() {
+  const select = document.getElementById("selectOptions").value;
+  if (select === "None") {
+    lactose = true;
+    nut = true;
+  } else if (select === "Lactose free") {
+    lactose = false;
+    nut = true;
+  } else if (select === "Nut free") {
+    lactose = true;
+    nut = false;
+  } else {
+    lactose = false;
+    nut = false;
+  }
+}
+
+/**
+ * Switch Organic and Non-Organic
+ * @param {Boolean} value
+ */
+function organicSwitcher(value) {
+  organic = value;
+}
+
+function populateProductFields() {
+  const productDiv = document.getElementById("productView");
+  const productsHere = filterProducts();
+
+  while (productDiv.firstChild) {
+    productDiv.removeChild(productDiv.firstChild);
+  }
+
+  for (let index = 0; index < productsHere.length; index++) {
+    const element = productsHere[index];
+
+    const listItemDiv = document.createElement("div");
+    listItemDiv.classList.add("listitem");
+
+    const imgTitleDiv = document.createElement("div");
+    imgTitleDiv.classList.add("itemTitle");
+
+    const img = document.createElement("img");
+    img.src = element.image;
+    img.alt = element.name;
+    img.width = 48;
+    img.height = 48;
+
+    const titleDiv = document.createElement("div");
+    titleDiv.innerText = element.name;
+    titleDiv.style.marginLeft = "10px";
+
+    imgTitleDiv.appendChild(img);
+    imgTitleDiv.appendChild(titleDiv);
+
+    const addButton = document.createElement("button");
+    addButton.id = index;
+    addButton.classList.add("btn");
+    if (cart.indexOf(element) === -1) {
+      addButton.classList.add("addBtn");
+      addButton.innerText = "Add To Cart";
+      addButton.onclick = () => {
+        addToCart(element, index);
+      };
+    } else {
+      addButton.classList.add("added");
+      addButton.innerText = "Added to cart";
+      addButton.onmouseenter = () => {
+        addButton.innerText = "Remove From Cart";
+      };
+      addButton.onmouseleave = () => {
+        addButton.innerText = "Added to cart";
+      };
+      addButton.onclick = () => {
+        removeFromCart(element, index);
+      };
+    }
+
+    listItemDiv.appendChild(imgTitleDiv);
+    listItemDiv.appendChild(addButton);
+
+    productDiv.appendChild(listItemDiv);
+  }
+}
+
+/**
+ *
+ * @param {object} product
+ * @param {string} btnId
+ */
+function addToCart(product, btnId) {
+  cart = [...cart, product];
+  const btn = document.getElementById(btnId);
+  btn.classList.add("added");
+  btn.innerText = "Added to cart";
+  btn.onmouseenter = () => {
+    btn.innerText = "Remove From Cart";
+  };
+  btn.onmouseleave = () => {
+    btn.innerText = "Added to cart";
+  };
+  btn.onclick = () => {
+    removeFromCart(product, btnId);
+  };
+
+  document.getElementById("cartNumbers").innerText = `(${cart.length})`;
+}
+
+/**
+ * @param {object} product
+ * @param {String} btnId
+ */
+function removeFromCart(product, btnId) {
+  const btn = document.getElementById(btnId);
+
+  btn.classList.remove("added");
+  btn.classList.add("addBtn");
+  btn.innerText = "Add To Cart";
+  btn.onclick = () => {
+    addToCart(product, btnId);
+  };
+  btn.onmouseenter = () => {};
+  btn.onmouseleave = () => {};
+
+  cart = cart.filter((value, index) => {
+    if (value.name !== product.name) {
+      return true;
+    }
+  });
+
+  document.getElementById("cartNumbers").innerText = `(${cart.length})`;``
+}
+
+
+function createReceiptBody() {
+  const body = document.getElementById("receiptBody");
+
+  while(body.firstChild) {
+    body.removeChild(body.firstChild);
+  }
+
+  for(let index = 0; index < cart.length; index++) {
+    const element = cart[index];
+
+    const listItem = document.createElement("div");
+    listItem.classList.add("receiptListItem");
+
+    const divName = document.createElement("div");
+    divName.innerText = element.name;
+
+    const divPrice = document.createElement("div");
+    divPrice.innerText = `$ ${element.price.toFixed(2)}`
+
+    listItem.appendChild(divName);
+    listItem.appendChild(divPrice);
+
+    body.appendChild(listItem);
+  }
+}
+
+function createReceiptFooter() {
+  const footer = document.getElementById("receiptFooter");
+  while(footer.firstChild) {
+    footer.removeChild(footer.firstChild);
+  }
+  const totalP = document.createElement("div");
+  totalP.innerText = "Total: ";
+
+  const total = document.createElement("div");
+  total.innerText = `$ ${calculateTotal().toFixed(2)}`;
+
+  footer.appendChild(totalP);
+  footer.appendChild(total);
+}
